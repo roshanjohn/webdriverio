4e62e3567af277399d0cb6762511ebf961707cce
Roshan John <roshanmathew.john@dnata.com>
2022-08-11 09:10:21 +0400

    Add additional assertions for wdio cloud Saucelabs execution

--- e2e/wdio/cloud/test.e2e.ts
+++ e2e/wdio/cloud/test.e2e.ts
@@ -1,5 +1,8 @@
 describe('this browser session', () => {
     it('should run in SL', () => {
-        expect(browser.options.hostname).toContain('saucelabs.com')
+        expect(browser.options.hostname).toContain('saucelabs.com');
+        expect(browser.options.protocol).toEqual('https');
+        expect(browser.options.user).toEqual(process.env.SAUCE_USERNAME);
+        expect(browser.options.key).toEqual(process.env.SAUCE_ACCESS_KEY);
     })
 })
